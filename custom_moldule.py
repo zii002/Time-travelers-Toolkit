ef4c027811ef16fe1bc62f89048a6ad453461962
Zeynep Akkurt <zeynepakkurt@gmail.com>
2026-03-23 20:23:53 +0300

    Add function to generate time travel message

---
+++ custom_moldule.py
@@ -0,0 +1,3 @@
+def generate_time_travel_message(year,destination,cost):
+  message='Welcome to the year '+ str(year) +' Your destination is '+ str(destination)+ ' the cost for this trip is '+ str(cost) + '!'
+  return message
